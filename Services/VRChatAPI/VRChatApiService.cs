1e95a9c2ad7582705d0d726a33d42b1b603d894a
shinyflvre <me@shinyflvres.com>
2026-04-04 12:21:36 +0200

    Bump. i reverted something locally my misstake!

--- Services/VRChatAPI/VRChatApiService.cs
+++ Services/VRChatAPI/VRChatApiService.cs
@@ -824,7 +824,7 @@ public class VRChatApiService
         using var client = new HttpClient();
         client.Timeout = TimeSpan.FromSeconds(15);
         client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", UA);
-        client.DefaultRequestHeaders.TryAddWithoutValidation("Referer", "https://vrcx.app");
+        client.DefaultRequestHeaders.TryAddWithoutValidation("Referer", $"https://{AppInfo.Website}");
         client.DefaultRequestHeaders.Accept.ParseAdd("application/json");
         try
         {
